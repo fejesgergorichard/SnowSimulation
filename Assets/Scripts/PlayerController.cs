8e5cf09552bce20acd45da9284f948f08161d832
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-02-23 17:03:17 +0100

    Control test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal * Speed, 0f, vertical * Speed);
+
+        transform.Translate(movement, Space.Self);
+
+    }
+}
